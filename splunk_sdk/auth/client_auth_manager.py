1eb3396bf06232c11355284f4957f75ea4370686
Dan Nguyen <dnguyen@dnguyen.sv.splunk.com>
2019-01-22 16:32:50 -0800

    Refactor Auth Manager

---
+++ splunk_sdk/auth/client_auth_manager.py
@@ -0,0 +1,27 @@
+from splunk_sdk.auth.auth_manager import AuthManager, AuthContext, \
+    DEFAULT_SCOPE
+from splunk_sdk.auth.okta_client import OktaClient
+
+
+class ClientAuthManager(AuthManager):
+
+    def __init__(self, host, client_id, client_secret, server,
+                 scope=DEFAULT_SCOPE):
+
+        super().__init__(host, client_id, server)
+        self.client_secret = client_secret
+        self.scope = scope
+        self.app = self._build_app_payload()
+
+    def _build_app_payload(self):
+        app = dict()
+        app['scope'] = self.scope
+        app['client_id'] = self.client_id
+        app['client_secret'] = self.client_secret
+        app['server'] = self.server
+        return app
+
+    def authenticate(self):
+        """Return the payload from okta, all tokens, expiry, etc"""
+        data = OktaClient(host=self.host).client(self.app)
+        return AuthContext(**data)
