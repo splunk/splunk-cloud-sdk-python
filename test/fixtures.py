aef500c03841325f56399843121f2750d0d48d2c
Dan Nguyen <dnguyen@splunk.com>
2019-01-24 22:54:40 +0000

    WIP testing credentials flow

--- test/fixtures.py
+++ test/fixtures.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 
+from splunk_sdk.auth.client_auth_manager import ClientAuthManager
 from splunk_sdk.auth.pkce_auth_manager import PKCEAuthManager
 from splunk_sdk.common.context import Context
 from splunk_sdk.base_client import get_client
@@ -18,8 +19,13 @@ def get_test_client():
 
 
 @pytest.fixture(scope="session")
-def get_auth_manager():
-    auth_manager = _get_pkce_manager()
+def get_auth_manager(authflow='pkce'):
+
+    if authflow == "pkce":
+        auth_manager = _get_pkce_manager()
+    else:
+        auth_manager = _get_client_manager()
+
     assert(auth_manager is not None)
     return auth_manager
 
@@ -31,3 +37,11 @@ def _get_pkce_manager():
                            username=os.environ.get('SPLUNK_USERNAME'),
                            password=os.environ.get('SPLUNK_PASSWORD'),
                            redirect_uri='http://localhost')
+
+
+def _get_client_manager():
+    return ClientAuthManager(host=os.environ.get('SPLUNK_AUTH_HOST'),
+                             client_id=os.environ.get('SPLUNK_APP_CLIENT_CRED_ID'),
+                             client_secret=os.environ.get('SPLUNK_APP_CLIENT_CRED_SECRET'),
+                             server=os.environ.get('SPLUNK_APP_SERVER')
+                             )
