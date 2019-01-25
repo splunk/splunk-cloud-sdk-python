b009484f3a52618615065c920a9d71629c04b25f
Shilpa Bijam <sbijam@splunk.com>
2019-01-25 14:40:52 -0800

    formatted input in test

--- test/test_ingest.py
+++ test/test_ingest.py
@@ -7,7 +7,13 @@ from splunk_sdk.ingest.results import Event
 @pytest.mark.usefixtures("test_client")  # NOQA
 def test_post_events(test_client):
     ingest = Ingest(test_client)
-    eventdata = {'host': 'host1', 'body': 'event1', 'source': 'source1', 'sourcetype': 'sourcetype1', 'timestamp': '1533671808138', 'nanos': 0, 'attributes': None}
+    eventdata = {'host': 'host1',
+                 'body': 'event1',
+                 'source': 'source1',
+                 'sourcetype': 'sourcetype1',
+                 'timestamp': '1533671808138',
+                 'nanos': 0,
+                 'attributes': None}
     arr = [eventdata, eventdata]
     event = ingest.post_events(arr)
     print("status", event)
