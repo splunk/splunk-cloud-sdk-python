4463f19481385705f25da1c304f141f2994b0956
Dan Nguyen <dnguyen@splunk.com>
2019-01-25 14:51:26 -0800

    Added more tests

--- test/auth/test_pkce_auth_manager.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from test.fixtures import get_auth_manager as auth_manager  # NOQA
-
-
-@pytest.mark.usefixtures('auth_manager')  # NOQA
-def test_authenticate(auth_manager):
-    auth_context = auth_manager.authenticate()
-    assert(auth_context is not None)
