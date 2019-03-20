d43382850fe43349a84a82002478666076568111
Liying Jiang <ljiang@splunk.com>
2019-03-20 10:28:08 -0700

    remove ssl_verify property as we always want it to be true

--- splunk_sdk/common/context.py
+++ splunk_sdk/common/context.py
@@ -11,12 +11,11 @@ class Context(object):
 
     def __init__(self, host=DEFAULT_HOST, api_host=DEFAULT_API_HOST,
                  app_host=DEFAULT_APP_HOST, port=None, scheme="https",
-                 ssl_verify=False, tenant='system', session=False):
+                 tenant='system', session=False):
         self.host = host
         self.api_host = api_host
         self.app_host = app_host
         self.port = port
         self.scheme = scheme
-        self.ssl_verify = ssl_verify  # TODO(dan): not currently used
         self.tenant = tenant
         self.session = session
