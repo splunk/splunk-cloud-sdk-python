1f63cce20624d320157919e57aa44f7bff7ec08a
Dan Nguyen <dnguyen@splunk.com>
2019-09-05 16:20:20 +0000

    Integrates docmd

---
+++ scripts/generate_docs.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")/../splunk_sdk"
+
+rm -rf ../docs
+mkdir -p ../docs/pkg
+
+DOC_INDEX="
+
+#  github.com/splunk/splunk-cloud-sdk-python
+
+## Packages
+
+"
+
+# generate md for directory modules under splunk_sdk
+for module in $(ls);
+do
+    # ignore files/dirs that start with _ or end with .py
+    if [[ ! "$module" == _* ]] && [[ ! "$module" == *.py ]]; then
+        docmd "splunk_sdk.$module" > "../docs/pkg/$module.md"
+        DOC_INDEX+="* [$module](pkg/$module.md)
+"
+    fi
+done
+
+echo "$DOC_INDEX" > ../docs/README.md
+
+cd "$(dirname "$0")/../"
+tar cvzf splunk_sdk.tar.gz docs
