17f00c14812364073f86be3a65f59ca374d0c17e
Liying Jiang <ljiang@splunk.com>
2020-04-14 15:36:10 -0700

    change doc generation to use pdoc

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,29 +1,6 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")/../splunk_sdk"
+rm -rf docs/pkg
+mkdir -p docs/pkg
 
-rm -rf ../docs/pkg
-rm ../docs/README.md
-rm ../docs/package.json
-mkdir -p ../docs/pkg
-
-DOC_INDEX="
-
-#  github.com/splunk/splunk-cloud-sdk-python
-
-## Packages
-
-"
-
-# generate md for directory modules under splunk_sdk
-for module in $(ls);
-do
-    # ignore files/dirs that start with _ or end with .py
-    if [[ ! "$module" == _* ]] && [[ ! "$module" == *.py ]]; then
-        docmd "splunk_sdk.$module" > "../docs/pkg/$module.md"
-        DOC_INDEX+="* [$module](pkg/$module.md)
-"
-    fi
-done
-
-echo "$DOC_INDEX" > ../docs/README.md
+pdoc --output-dir docs/pkg splunk_sdk
