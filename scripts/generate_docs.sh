d1e7225aa744602026bca687cef42a87a7247b87
Liying Jiang <ljiang@splunk.com>
2020-04-15 15:01:42 -0700

    fix hyperlink in README.md

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env bash
 
-rm -rf docs/pkg
-mkdir -p docs/pkg
+rm -rf docs
+mkdir -p docs
 
-pdoc --output-dir docs/pkg splunk_sdk
+pdoc --output-dir docs splunk_sdk
+
+cd docs/splunk_sdk
+
+DOC_INDEX="
+
+#  github.com/splunk/splunk-cloud-sdk-python
+
+## Packages
+
+"
+
+# generate md for directory modules under splunk_sdk
+for module in $(ls); do
+  for spec_version in $(ls $module); do
+    if [[ "$spec_version" == "index.md" ]]; then
+      continue
+    fi
+
+    for file_name in $(ls $module/$spec_version); do
+      if [[ "$file_name" == "index.md" ]]; then
+        continue
+      fi
+      DOC_INDEX+="* [$module/$spec_version/$file_name](splunk_sdk/$module/$spec_version/$file_name)
+"
+    done
+  done
+done
+
+echo "$DOC_INDEX" >../../docs/README.md
